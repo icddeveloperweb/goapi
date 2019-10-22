e75316e8ccae4922a9c3812a4f252ac45fa96db0
icddeveloperweb <56845153+icddeveloperweb@users.noreply.github.com>
2019-10-22 02:20:40 -0700

    Add homeAPI, POST, GET, PATCH and DELETE

--- main.go
+++ main.go
@@ -8,19 +8,20 @@ import (
 )
 
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+func homeAPI(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the API!")
 }
 
 func handleRequest() {
-	//var PORT := 8082
+	var PORT = ":8082"
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", helloWorld).Methods("GET")
-	myRouter.HandleFunc("/users", AllUsers).Methods("GET")
-	myRouter.HandleFunc("/user/add", NewUser).Methods("POST")
-	myRouter.HandleFunc("/user/delete", DeleteUser).Methods("DELETE")
-	myRouter.HandleFunc("/user/update", UpdateUser).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":8082",myRouter))
+	myRouter.HandleFunc("/api", homeAPI).Methods("GET")
+	myRouter.HandleFunc("/api/users", AllUsers).Methods("GET")
+	myRouter.HandleFunc("/api/user/{id}", UserbyID).Methods("GET")
+	myRouter.HandleFunc("/api/user", NewUser).Methods("POST")
+	myRouter.HandleFunc("/api/user/{id}", DeleteUser).Methods("DELETE")
+	myRouter.HandleFunc("/api/user/{id}", UpdateUser).Methods("PATCH")
+	log.Fatal(http.ListenAndServe(PORT,myRouter))
 }
 
 func main() {
